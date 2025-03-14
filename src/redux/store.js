c493b12abb4972e53433fde9b907943e87306c1b
detrujamilan <milandetruja2@gmail.com>
2025-03-14 22:23:00 +0530

    Design Issue Resolved

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import timerReducer from './slices/timerSlice';
+
+const store = configureStore({
+  reducer: {
+    timer: timerReducer,
+  },
+});
+
+export default store;
